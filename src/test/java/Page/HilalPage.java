97f5dd93346df0a637006bcd6e7fb5e6cec61b2c
hilaldkmn <dikmanhilal@gmail.com>
2025-05-30 18:17:00 +0300

    first stepdefinitions and page configuration

--- src/test/java/Page/HilalPage.java
+++ src/test/java/Page/HilalPage.java
@@ -1,6 +1,17 @@
 package Page;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
 
 public class HilalPage {
 
-    //hilal
+    public HilalPage(){
+        PageFactory.initElements(new AppiumFieldDecorator(Driver.getAppiumDriver()),this);
+    }
+
+    @FindBy(xpath = "//android.widget.ImageView[@content-desc='Category']")
+    public WebElement CategoryButton;
+
 }
--- src/test/java/stepdefinitions/HilalStepDef.java
+++ src/test/java/stepdefinitions/HilalStepDef.java
@@ -1,4 +1,18 @@
 package stepdefinitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import Page.HilalPage;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
 public class HilalStepDef {
+    HilalPage page=new HilalPage();
+
+    @Given("User opens the application")
+    public void openApp() {
+        Driver.getAppiumDriver();
+    }
 }
